84e4f2644ae9060f242e24beb48a9c525e5bb435
unknown <igbokwefc1@gmail.com>
2025-09-07 12:22:28 +0430

    sign up page 4

--- components/layout/TopSection.jsx
+++ components/layout/TopSection.jsx
@@ -23,7 +23,7 @@ const TopSection = () => {
         className={`fixed top-0 left-0 right-0 flex justify-between items-center py-4 px-[162px] border-b border-[#2FC22B80] transition-all duration-300 z-50 
           ${scrolled ? "bg-black/60 backdrop-blur-md" : "bg-transparent"}`}
       >
-        <Image src="/logo.png" width={86} height={29} alt="logo" />
+        <Image src="/logo.svg" width={86} height={29} alt="logo" />
         <Link href={'/'} className="text-[#2FC22B] text-[20px] font-[400] cursor-pointer">
           Cancel
         </Link>
